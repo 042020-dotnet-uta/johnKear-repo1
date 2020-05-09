7de44f870c9abb799fb99bd3dadf15893daf53a7
agony84 <jjk198478@gmail.com>
2020-05-09 15:33:38 -0600

    creates proj1OnlineStore

---
+++ proj1_OnlineStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proj1_OnlineStore.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
