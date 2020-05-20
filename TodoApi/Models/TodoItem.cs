35b2a3851d72906273a44301690bb4107eaaae4e
agony84 <jjk198478@gmail.com>
2020-05-20 15:55:56 -0600

    adds todoAPI tutorial

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+		public string Secret { get; set; }
+        public virtual Classification Classification { get; set; }
+    }
+
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+    public class Classification
+	{
+		public long Id { get; set; }
+		public string Type { get; set; }
+		
+	}
+
+}
